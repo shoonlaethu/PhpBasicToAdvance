0c45ee0725ffecab9360dde021714a035eaea452
Shoon Lae Thu <107358135+shoonlaethu@users.noreply.github.com>
2024-05-20 01:04:03 +0630

    Update defineFun().php

--- php advance/1_dateTime&defineFun/defineFun().php
+++ php advance/1_dateTime&defineFun/defineFun().php
@@ -1,3 +1,26 @@
-$name = "Thaw Khant";  // local scope in php sir
-    $arr = ["mango","apple","orange"];  
-    define("NAME", $arr,false);  // it look like const in javascript  // a nout mar case sensitive shi tay dal but i have a little error // defalult ka false
+<!--define(name, value, case_insensitive);
+//name: Specifies the name of the constant.
+//value: Specifies the value of the constant.
+//case_insensitive: Optional. Specifies whether the constant name should be case-insensitive. Default is false. -->
+<?php
+
+// Define a constant
+define("PI", 3.14);
+// Use the constant
+echo "The value of PI is " . PI;
+
+
+// Define a case-insensitive constant
+define("GREETING", "Hello, World!", true);
+// Use the constant with different case variations
+echo "The greeting is " . greeting;
+echo "The greeting is " . GREETING;
+//The greeting is Hello, World!
+//The greeting is Hello, World!
+
+
+
+
+
+?>
+
